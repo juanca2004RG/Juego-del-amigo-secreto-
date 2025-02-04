f41f91c4ef369fea9db9a26bce84c379bb7467b1
juanca2004RG <juangarcia200586@gmail.com>
2025-02-03 23:55:33 -0500

    finalizacion del proyecto

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
 let listaAmigos = []
-// funcion de dimplificasion de modificacion de elementos html
+// funcion de simplificasion de modificacion de elementos html
 function asignarNombreElemento (elemento,texto){
     let elementoHTML = document.querySelector(elemento);
     elementoHTML.innerHTML = texto;
@@ -11,7 +11,7 @@ function agregarAmigo () {
     let amigosUsuario = document.getElementById ("amigo").value.trim();
     //varificacion de campo vasio
     if (amigosUsuario=== "") {
-        alert("Porfavor ingrese un nombre valido");
+        alert("Porfavor, ingrese un nombre valido");
         return;
     }
     //agregar a la lista 
@@ -25,12 +25,19 @@ function agregarAmigo () {
 function limpiarbarra  () {
     document.querySelector('#amigo'). value =''
 }
+//Detecciond de la tecla enter 
+document.getElementById('amigo').addEventListener('keydown', function(event) {
+    if (event.key === 'Enter') {
+        agregarAmigo(); // Llamar a la función para agregar el amigo
+        limpiarbarra(); // Limpiar el campo para que el usuario pueda escribir nuevamente
+    }
+});
 //funcion de actualizacon de lista de amigo html 
 function actualizarListaAmigos(){
     let listahtml = document.getElementById("listaAmigos");
     listahtml.innerHTML= "";
 
-    //bucle for para recorrer lita con amyor control 
+    //bucle for para recorrer lita con mayor control 
     for(let i = 0; i < listaAmigos.length; i++){
         let amigo = listaAmigos [i];
         //creacion de <li> para cada amigo 
@@ -40,3 +47,37 @@ function actualizarListaAmigos(){
         listahtml.appendChild(li);
     }
 }
+//Fuoncion de soteo de amigo 
+function amigosorteado (){
+    if (listaAmigos.length ===0){
+        alert ("Lista sin amigos, por favor dijite el nombre");
+        return;
+    }
+    //asignacion aleatoria de amigo secreto 
+    let selecionAleatoria = Math.floor (Math.random() * listaAmigos.length);
+    //Optecion de nombre de amiog secreto 
+    let amigoEscogido = listaAmigos[selecionAleatoria];
+
+    //Mostrar gandor en pantalla (html)
+    asignarNombreElemento ('#resultado', `Felicidades el  amigo secreto es: ${amigoEscogido}`);
+
+}
+//Fujncion para las condiciones inicales 
+function condicionesIniciales(){
+    //vaciar el resultado del sorteo 
+    asignarNombreElemento("#resultado", "");
+    //liampia campo de entrada 
+    document.querySelector ("#amigo ").value = "";
+    //vaciar lista de amigos en pantalla
+    document.getElementById ("listaAmigos").innerHTML = "";
+    //Limpira el array de amigos 
+    listaAmigos = [];
+}
+function reiniciarJuego(){
+    condicionesIniciales()
+        document.querySelector(".button-drew").removeAttribute("diseabled");
+    
+}
+
+//Ejecucuon de condiciones iniciales al cargar la pagina 
+condicionesIniciales();
